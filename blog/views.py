a6c1952e6d8f832ee4f0b6673da5c71f6ea6d101
seong-hui <msh429062@gmail.com>
2021-01-14 03:24:58 +0900

    어플리케이션_확장하기

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone  # timezone 모듈 불러오기
 from .models import Post
-# Create your views here.
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
         'published_date')  # 블로그 글목록 게시일 기준 정렬
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
