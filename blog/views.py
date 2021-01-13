907b781e8ec6aeda1c0b249b164015610ab7bff5
seong-hui <msh429062@gmail.com>
2021-01-14 02:26:36 +0900

    템플릿_동적_데이터

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from django.utils import timezone  # timezone 모듈 불러오기
+from .models import Post
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+        'published_date')  # 블로그 글목록 게시일 기준 정렬
+    return render(request, 'blog/post_list.html', {'posts': posts})
